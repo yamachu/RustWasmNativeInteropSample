c38199fcd03696baec4f8b89018c628caf96ee2d
yamachu <yamachu.dev@gmail.com>
2022-11-01 02:36:06 +0900

    buildのcfgによって実装が変わるようになった

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,11 +1,34 @@
 mod text_to_label;
 
-use text_to_label::{implement, shared::TextToLabel};
+use text_to_label::{
+    implement::{self, OpenJTalk},
+    shared::TextToLabel,
+};
+
+struct SampleImpl {
+    ojt: OpenJTalk,
+}
+
+impl SampleImpl {
+    pub fn initialize() -> Self {
+        Self {
+            ojt: implement::OpenJTalk::initialize(),
+        }
+    }
+
+    pub fn run(&mut self) {
+        if let Ok(v) = self.ojt.extract_fullcontext("ほげふが") {
+            println!("{:?}", v);
+        }
+    }
+}
 
 pub fn dummy_impl() {
-    if let Ok(v) = implement::OpenJTalkNative::initialize().extract_fullcontext("ほげふが") {
+    if let Ok(v) = implement::OpenJTalk::initialize().extract_fullcontext("ほげふが") {
         println!("{:?}", v);
     }
+
+    SampleImpl::initialize().run();
 }
 
 // TODO: Test Native Library
